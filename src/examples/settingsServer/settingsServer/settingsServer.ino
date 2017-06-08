fe9c22313545124d5b4a4622f3aba278caf9d5a3
Gert <gert@vantslot.be>
2017-06-08 22:56:44 +0200

    Settings example

---
+++ src/examples/settingsServer/settingsServer/settingsServer.ino
@@ -0,0 +1,19 @@
+/*
+ Name:		settingsServer.ino
+ Created:	6/6/2017 11:04:54 PM
+ Author:	Gert
+*/
+
+// the setup function runs once when you press reset or power the board
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <ESPAsyncWebServer.h>
+
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
