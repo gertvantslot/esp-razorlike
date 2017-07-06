7d2afb1e1100c17001a69d6d95632db5a3cff8e1
Gert <gert@vantslot.be>
2017-07-06 22:15:04 +0200

    Settings example is more complete

--- src/examples/settingsServer/settingsServer/razor_impl.ino
+++ src/examples/settingsServer/settingsServer/razor_impl.ino
@@ -6,31 +6,43 @@ void index_cshtml(AsyncWebServerRequest *request) {
  
     onIndexRequest(request);
 
-  _razor_fromFile(response, &file, 39, 504);
+  _razor_fromFile(response, &file, 39, 341);
   _razor_fromExpression(response, (settings.page_title));
 
-  _razor_fromFile(response, &file, 526, 997);
+  _razor_fromFile(response, &file, 363, 909);
+  _razor_fromExpression(response, (settings.wifi_sta ? "" : "checked"));
+
+  _razor_fromFile(response, &file, 946, 1150);
+  _razor_fromExpression(response, (settings.wifi_sta ? "checked" : ""));
+
+  _razor_fromFile(response, &file, 1187, 1466);
+  _razor_fromExpression(response, (settings.ssid_name));
+
+  _razor_fromFile(response, &file, 1487, 2499);
+  _razor_fromExpression(response, (settings.emergency_ssid_name));
+
+  _razor_fromFile(response, &file, 2530, 3187);
   _razor_fromExpression(response, (settings.page_title));
 
-  _razor_fromFile(response, &file, 1019, 1484);
+  _razor_fromFile(response, &file, 3209, 3667);
   _razor_fromExpression(response, (settings.led_red ? "checked" : ""));
 
-  _razor_fromFile(response, &file, 1520, 1729);
+  _razor_fromFile(response, &file, 3703, 3922);
   _razor_fromExpression(response, (settings.led_green ? "checked" : ""));
 
-  _razor_fromFile(response, &file, 1767, 1973);
+  _razor_fromFile(response, &file, 3960, 4176);
   _razor_fromExpression(response, (settings.led_blue ? "checked" : ""));
 
-  _razor_fromFile(response, &file, 2010, 2594);
+  _razor_fromFile(response, &file, 4213, 4785);
   _razor_fromExpression(response, settings.light_min);
 
-  _razor_fromFile(response, &file, 2613, 2855);
+  _razor_fromFile(response, &file, 4804, 5046);
   _razor_fromExpression(response, settings.light_max);
 
-  _razor_fromFile(response, &file, 2874, 3201);
+  _razor_fromFile(response, &file, 5065, 5392);
   _razor_fromExpression(response, settings.email);
 
-  _razor_fromFile(response, &file, 3216, 4377);
+  _razor_fromFile(response, &file, 5407, 6333);
   file.close(); 
   request->send(response);
 }
--- src/examples/settingsServer/settingsServer/settingsServer.ino
+++ src/examples/settingsServer/settingsServer/settingsServer.ino
@@ -11,6 +11,9 @@
 #include <EEPROM.h>
 
 AsyncWebServer server(80);
+bool reboot = false;
+bool wifi_emergency = false;
+unsigned long emergency_mode_end;
 
 // CalculateCRC32 from https://github.com/esp8266/Arduino/blob/master/libraries/esp8266/examples/RTCUserMemory/RTCUserMemory.ino 
 uint32_t calculateCRC32(const uint8_t *data, size_t length)
@@ -34,6 +37,11 @@ uint32_t calculateCRC32(const uint8_t *data, size_t length)
 
 #define SETTINGS_ADDR 0
 struct Settings {
+	bool wifi_sta;
+	char ssid_name[100];
+	char ssid_password[100];
+	char emergency_ssid_name[100];
+	char emergency_ssid_password[100];
 	bool led_red;
 	bool led_green;
 	bool led_blue;
@@ -45,8 +53,16 @@ struct Settings {
 };
 
 Settings settings;
+#define SSID_NAME_DEFAULT "example"
+#define SSID_PASSWORD_DEFAULT "example password ##"
 
 void initSettings() {
+	settings.wifi_sta = false;
+	strcpy(settings.ssid_name, SSID_NAME_DEFAULT);
+	strcpy(settings.ssid_password, SSID_PASSWORD_DEFAULT);
+	strcpy(settings.emergency_ssid_name, SSID_NAME_DEFAULT);
+	strcpy(settings.emergency_ssid_password, SSID_PASSWORD_DEFAULT);
+
 	settings.led_red = settings.led_green = settings.led_blue = false;
 	settings.light_min = 20;
 	settings.light_max = 50;
@@ -67,27 +83,19 @@ bool getSettingsFromEEPROM() {
 	struct Settings buffer;
 	EEPROM.get(SETTINGS_ADDR, buffer);
 	uint32 storedCRC = buffer.crc32;
-	Serial.print("Received CRC:");
-	Serial.println(buffer.crc32);
 
 	updateSettingsCRC(&buffer);
-	Serial.print("calculated CRC:");
-	Serial.println(buffer.crc32);
 	uint32 calculatedCRC = buffer.crc32;
 	if (storedCRC == calculatedCRC) {
-		Serial.println("Valid settings found in EEPROM");
 		// EEPROM contains a valid settings object
-		settings = buffer;
+		// settings = buffer;
+		memcpy(&settings, &buffer, sizeof(Settings));
 		return true;
 	} 
-	Serial.println("INVALID settings found in EEPROM - no update");
 	return false;
 }
 
 void putSettingsInEEPROM() {
-	Serial.println("Settings will be stored");
-	Serial.print("Putting CRC:");
-	Serial.println(settings.crc32);
 	EEPROM.put(SETTINGS_ADDR, settings);
 	EEPROM.commit();
 	// Retrieve to see if correctly stored
@@ -103,7 +111,6 @@ bool settingsNeedUpdate() {
 void putSettingsInEEPROMifNeeded() {
 	updateSettingsCRC(&settings);
 	if (settingsNeedUpdate()) {
-		Serial.println("EEPROM settings update needed");
 		putSettingsInEEPROM();
 	}
 }
@@ -116,9 +123,11 @@ const int LED_B = D6; // Blue
 
 void setup() {
 	Serial.begin(115200);
+	Serial.println();
+	Serial.println("Example - Settings");
 
 	// Initialize electronics
-	EEPROM.begin(512);
+	EEPROM.begin(sizeof(Settings));
 	SPIFFS.begin();
 
 	pinMode(LED_R, OUTPUT);
@@ -127,22 +136,56 @@ void setup() {
 
 	// Retrieve settings if available and correct
 	initSettings();
-	getSettingsFromEEPROM();
+	if (getSettingsFromEEPROM()) {
+		Serial.println("Usable settings found in EEPROM");
+	}
 
 	// Initialize WiFi
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(); // Use cached SSID and password
-
-	// Wait for connection
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
+	if (settings.wifi_sta) {
+		Serial.println("Connection to AP");
+		WiFi.mode(WIFI_STA);
+		WiFi.begin(settings.ssid_name, settings.ssid_password); // Use cached SSID and password
+	
+		// Wait for connection
+		unsigned long maxWaitTime = millis() + 10000; // 10 seconds
+		while (WiFi.status() != WL_CONNECTED) {
+			delay(500);
+			Serial.print(".");
+			if (millis() > maxWaitTime) {
+				// Still no connection
+				Serial.println("no connection. Start emergency mode");
+				// In emergency mode, you are still able to connect to the device
+				// so you can control it, to connect it to another SSID.
+				// For safety, we use the stored password.
+				wifi_emergency = true;
+				emergency_mode_end = 10 * 60 * 1000; // retry normal ap in 10 minutes
+				break;
+			}
+		}
+		Serial.println();
+		Serial.println(WiFi.localIP());
 	}
-	Serial.println();
-	Serial.println(WiFi.localIP());
+	
+	if (wifi_emergency || !settings.wifi_sta) {
+		Serial.println("Starting AP mode");
+		char * ssid = wifi_emergency ? settings.emergency_ssid_name : settings.ssid_name;
+		char * password = wifi_emergency ? settings.emergency_ssid_password : settings.ssid_password;
+
+		WiFi.mode(WIFI_AP);
+		WiFi.softAP(ssid, password);
+		WiFi.begin();
+
+		Serial.print("SSID: ");
+		Serial.println(ssid);
+		Serial.println(WiFi.softAPIP());
+	}
+
 
 	razor_setup(&server);
 	server.on("/", index_cshtml);
+	server.serveStatic("/js/", SPIFFS, "/js/").setCacheControl("max-age=600");
+	server.serveStatic("/css/", SPIFFS, "/css/").setCacheControl("max-age=600");
+
 	server.on("/red", HTTP_GET, [](AsyncWebServerRequest *request) {
 		settings.led_red = !settings.led_red;
 		request->send(200, "text/plain", String(settings.led_red));
@@ -160,16 +203,47 @@ void setup() {
 
 void onIndexPost(AsyncWebServerRequest *request) {
 	strlcpy(settings.page_title, request->arg("page_title").c_str(), 50);
+	strlcpy(settings.email, request->arg("email").c_str(), 100);
+
 	settings.led_red = strncmp("on", request->arg("red").c_str(), 2) == 0;
 	settings.led_green = strncmp("on", request->arg("green").c_str(), 2) == 0;
 	settings.led_blue = strncmp("on", request->arg("blue").c_str(), 2) == 0;
+
 	settings.light_min = atoi(request->arg("light_min").c_str());
 	settings.light_max = atoi(request->arg("light_max").c_str());
-	strlcpy(settings.email, request->arg("email").c_str(), 100);
-	
+
+	// Wifi Settings
+
+	bool sta = strncmp("sta", request->arg("wifi_mode").c_str(), 3) == 0;
+	if (sta != settings.wifi_sta) {
+		settings.wifi_sta = sta;
+		reboot = true;
+	}
+	String name = request->arg("ssid_name");
+	if (strncmp(settings.ssid_name, name.c_str(), 100) != 0) {
+		strlcpy(settings.ssid_name, name.c_str(), 100);
+		reboot = true;
+	}
+	String password = request->arg("ssid_password");
+	if (password.length() != 0) {
+		strlcpy(settings.ssid_password, password.c_str(), 100);
+		reboot = true;
+	}
+
+	strlcpy(settings.emergency_ssid_name, request->arg("emergency_ssid_name").c_str(), 100);
+	String epassword = request->arg("emergency_ssid_password");
+	if (epassword.length() != 0) {
+		strlcpy(settings.emergency_ssid_password, epassword.c_str(), 100);
+	}
+
 	if (request->arg("submit").length() != 0) {
 		putSettingsInEEPROMifNeeded();
 	}
+
+	if (request->arg("reboot").length() != 0) {
+		reboot = true;
+	}
+
 }
 
 void onIndexRequest(AsyncWebServerRequest *request) {
@@ -183,4 +257,20 @@ void loop() {
 	digitalWrite(LED_R, settings.led_red ? HIGH : LOW);
 	digitalWrite(LED_G, settings.led_green);
 	digitalWrite(LED_B, settings.led_blue);
+
+	if (wifi_emergency && millis() > emergency_mode_end) {
+		reboot = true;
+	}
+
+	if (reboot) {
+		reboot = false;
+		Serial.println("bye");
+
+		// This will fail the first time after flashing.
+		// First do a manual reboot (button)
+		// See: https://github.com/esp8266/Arduino/issues/2876
+
+		ESP.reset();
+		Serial.println("I'm now a zombie");
+	}
 }
